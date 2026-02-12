3fe4da871b5f6cce47362b6d09e1c402839972f9
Craftmaker <shivamkjha23@gmail.com>
2026-02-12 15:57:37 +0530

    Delete js directory

--- js/storageService.js
+++
@@ -1,8 +0,0 @@
-import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js';
-import { storage } from './firebase.js';
-
-export async function uploadInspectionImage(file) {
-  const fileRef = ref(storage, `inspection-images/${Date.now()}-${file.name}`);
-  await uploadBytes(fileRef, file);
-  return getDownloadURL(fileRef);
-}
