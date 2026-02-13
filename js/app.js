f60647243c96b2b6c545f37dfccda4578ff8a59a
Craftmaker <shivamkjha23@gmail.com>
2026-02-13 19:07:06 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const UNIT_OPTIONS = ['GCU-1', 'GCU-2', 'GPU-1', 'GPU-2', 'HDPE-1', 'HDPE-2', 'LLDPE-1', 'LLDPE-2', 'PP-1', 'PP-2', 'LPG', 'SPHERE', 'YARD', 'FLAKER-1', 'BOG', 'IOP','LOADING GANTRY','SCPT','ANP','ANP-II','DNP-1',DNP-2','Butene-1','New Butene-1'];
+const UNIT_OPTIONS = ['','GCU-1', 'GCU-2', 'GPU-1', 'GPU-2', 'HDPE-1', 'HDPE-2', 'LLDPE-1', 'LLDPE-2', 'PP-1', 'PP-2', 'LPG', 'SPHERE', 'YARD', 'FLAKER-1', 'BOG', 'IOP','LOADING GANTRY','SCPT','ANP','ANP-II','DNP-1',DNP-2','Butene-1','New Butene-1'];
 const EQUIPMENT_TYPES = ['Pipeline', 'Vessel', 'Exchanger', 'Steam Trap', 'Tank'];
 const INSPECTION_STATUS_OPTIONS = ['Scaffolding Prepared', 'Manhole Opened', 'NDT in Progress', 'Insulation Removed', 'Manhole Box-up','Module Prepared'];
 
